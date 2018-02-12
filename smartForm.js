93d7f0797e36bc0cef12ef27f02864f6a20f2f23
cis101 <cis101@secs.iub.edu.bd>
2018-02-12 09:31:43 +0600

    class work

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,40 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+	name: '',
+	email: '',
+	html: {likes: [], dislikes: []},
+	css: {likes: [], dislikes: []},
+	js: {likes:[], dislikes: []},
+	strength: {css: '', js: '', html: ''},
+	currentQuestion: '#welcome'
+};
+
+$('#start').click(function() {
+	console.log('start');
+	$('#welcome').hide();
+	$('#q1').show();
+});
+
+$('#name').change(function(event) {
+	// console.log(event);
+});
+
+$('#q1next').click(function() {
+	if($('#name').val() && $('#email').val()) {
+		userData.name = $('#name').val();
+		userData.email = $('#email').val();
+		
+		$('#q2').show();
+		$('#q1').hide();
+
+		console.log($('#name').val());
+		console.log($('#email').val());
+	} else {
+		alert('Please enter name and email');
+	}
+});
+
+$('#htmlopen').click(function() {
+	$('#q2a').show();
+});
