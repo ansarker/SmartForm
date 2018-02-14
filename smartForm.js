91dea2db553a0a7e73ed548d1febf5423858e7ea
cis101 <cis101@secs.iub.edu.bd>
2018-02-14 09:34:06 +0600

    Assignment 7 & 8 not done yet

--- smartForm.js
+++ smartForm.js
@@ -10,20 +10,35 @@ var userData = {
 	currentQuestion: '#welcome'
 };
 
+if(localStorage.getItem('userData')) {
+	userData = JSON.parse(localStorage.getItem('userData'));
+	$(userData.currentQuestion).show();
+	$("#welcome").hide();
+	$("#name").val(userData.name);
+	$("#email").val(userData.email);
+	
+} else {
+	localStorage.setItem('userData', JSON.stringify(userData));
+}
+
 $('#start').click(function() {
 	console.log('start');
 	$('#welcome').hide();
 	$('#q1').show();
+	userData.currentQuestion = "#q1";
+	localStorage.setItem('userData', JSON.stringify(userData));
 });
 
 $('#name').change(function(event) {
-	// console.log(event);
+	console.log(event);
 });
 
 $('#q1next').click(function() {
 	if($('#name').val() && $('#email').val()) {
 		userData.name = $('#name').val();
 		userData.email = $('#email').val();
+
+		localStorage.setItem('userData', JSON.stringify(userData));
 		
 		$('#q2').show();
 		$('#q1').hide();
